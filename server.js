f146315a2320a16b5c05a835478f15de13f569b7
Aminbaklouti <87398130+Aminbaklouti@users.noreply.github.com>
2022-08-28 22:59:16 +0100

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 
-const whitelist = ['https://qm.anouarmaalej.com'];
+const whitelist = ['https://qm.anouarmaalej.com', 'http://localhost:3000'];
 const corsOptions = {
   credentials: true, 
   origin: (origin, callback) => {
