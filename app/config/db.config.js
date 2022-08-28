fbd3ce4c7bc19a7ce8a6e2ffaccd231b38472d87
Aminbaklouti <87398130+Aminbaklouti@users.noreply.github.com>
2022-08-28 22:08:40 +0100

    fix

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,15 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "admin",
-    PASSWORD: "admin",
-    DB: "QM",
-    dialect: "mysql"
-};
-
 // module.exports = {
-//     HOST: "sql208.main-hosting.eu",
-//     USER: "u664904340_QMDEV",
-//     PASSWORD: "WuzgJ]Si9#",
-//     DB: "u664904340_QM",
+//     HOST: "localhost",
+//     USER: "admin",
+//     PASSWORD: "admin",
+//     DB: "QM",
 //     dialect: "mysql"
 // };
+
+module.exports = {
+    HOST: "sql208.main-hosting.eu",
+    USER: "u664904340_QMDEV",
+    PASSWORD: "WuzgJ]Si9#",
+    DB: "u664904340_QM",
+    dialect: "mysql"
+};
