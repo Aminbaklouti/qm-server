8bbe2c3b0e9c691b5fd5c2b390e9e26e01c9afa9
Aminbaklouti <87398130+Aminbaklouti@users.noreply.github.com>
2022-08-29 17:08:20 +0100

    test

--- app/src/controllers/JWTController.js
+++ app/src/controllers/JWTController.js
@@ -2,7 +2,19 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     sign(req,res,user){
-        req.cookies['test'] = 'test123';
+        
+        var cookie = req.cookies.cookieName;
+        if (cookie === undefined) {
+            // no: set a new cookie
+            var randomNumber=Math.random().toString();
+            randomNumber=randomNumber.substring(2,randomNumber.length);
+            res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+            console.log('cookie created successfully');
+        } else {
+            // yes, cookie was already present 
+            console.log('cookie exists', cookie);
+        } 
+
         jwt.sign({user}, process.env.JWTSECRET, {expiresIn: '1d'}, (err,token)=>{
             if(err){
                 res.status(400); 
