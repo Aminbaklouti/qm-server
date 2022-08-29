23fd0b7927a257fe2ea93a19777c6c24ae98c3f5
Aminbaklouti <87398130+Aminbaklouti@users.noreply.github.com>
2022-08-29 13:54:38 +0100

    test

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 
-// const whitelist = ['https://qm.anouarmaalej.com', 'http://localhost:3000'];
-// const corsOptions = {
-//   credentials: true, 
-//   origin: (origin, callback) => {
-//     if(whitelist.includes(origin))
-//       return callback(null, true)
+const whitelist = ['https://qm.anouarmaalej.com', 'http://localhost:3000'];
+const corsOptions = {
+  credentials: true, 
+  origin: (origin, callback) => {
+    return callback(null, true)
+    if(whitelist.includes(origin))
+    
+      callback(new Error('Not allowed by CORS'));
+  }
+}
 
-//       callback(new Error('Not allowed by CORS'));
-//   }
-// }
-
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.static(path.resolve(__dirname, '../qm-frontend/build')));
 
